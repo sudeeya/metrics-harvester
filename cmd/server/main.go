c8c776537adb6e9fba4b1ee823a958bb9344986b
sudeeya <pyrkin.aa@phystech.edu>
2024-07-25 17:52:50 +0300

    Change project structure and refactor the code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,61 +2,15 @@ package main
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
 
-	repo "github.com/sudeeya/metrics-harvester/internal/repository"
 	"github.com/sudeeya/metrics-harvester/internal/repository/storage"
+	"github.com/sudeeya/metrics-harvester/internal/router"
 )
 
 func main() {
 	memStorage := storage.NewMemStorage()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			path := r.URL.Path[len("/update/"):]
-			splitPath := strings.Split(path, "/")
-			switch splitPath[0] {
-			case "gauge":
-				if len(splitPath) < 3 {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				} else if len(splitPath) != 3 {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				metric, err := strconv.ParseFloat(splitPath[2], 64)
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				memStorage.PutGauge(splitPath[1], repo.Gauge(metric))
-			case "counter":
-				if len(splitPath) < 3 {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				} else if len(splitPath) != 3 {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				metric, err := strconv.ParseInt(splitPath[2], 0, 64)
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				memStorage.PutCounter(splitPath[1], repo.Counter(metric))
-			default:
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			w.Header().Set("content-type", "text/plain")
-			w.WriteHeader(http.StatusOK)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-		}
-	})
-	mux.HandleFunc("/", http.NotFound)
-	err := http.ListenAndServe(":8080", mux)
+	router := router.NewRouter(memStorage)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		panic(err)
 	}
---
+++ internal/router/router.go
@@ -0,0 +1,70 @@
+package router
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	repo "github.com/sudeeya/metrics-harvester/internal/repository"
+)
+
+type Router struct {
+	repository repo.Repository
+	mux        *http.ServeMux
+}
+
+func NewRouter(repository repo.Repository) *Router {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			path := r.URL.Path[len("/update/"):]
+			splitPath := strings.Split(path, "/")
+			switch splitPath[0] {
+			case "gauge":
+				if len(splitPath) < 3 {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				} else if len(splitPath) != 3 {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				metric, err := strconv.ParseFloat(splitPath[2], 64)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				repository.PutGauge(splitPath[1], repo.Gauge(metric))
+			case "counter":
+				if len(splitPath) < 3 {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				} else if len(splitPath) != 3 {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				metric, err := strconv.ParseInt(splitPath[2], 0, 64)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				repository.PutCounter(splitPath[1], repo.Counter(metric))
+			default:
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			w.Header().Set("content-type", "text/plain")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	mux.HandleFunc("/", http.NotFound)
+	return &Router{
+		repository: repository,
+		mux:        mux,
+	}
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router.mux.ServeHTTP(w, r)
+}
