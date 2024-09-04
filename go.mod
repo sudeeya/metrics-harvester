b680f510dd90d284ac4410194e020c9bb6a77bf7
sudeeya <pyrkin.aa@phystech.edu>
2024-09-05 00:10:23 +0300

    Change the order of config initialization

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-resty/resty/v2 v2.14.0
 	github.com/jackc/pgx/v5 v5.6.0
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
 )
@@ -16,7 +17,6 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- internal/agent/config.go
+++ internal/agent/config.go
@@ -7,27 +7,39 @@ import (
 	"github.com/caarlos0/env/v11"
 )
 
+const (
+	defaultAddress         string = "localhost:8080"
+	defaultBackoffSchedule string = "1,3,5"
+	defaultKey             string = ""
+	defaultLogLevel        string = "info"
+	defaultPollInterval    int64  = 2
+	defaultRateLimit       int64  = 16
+	defaultReportInterval  int64  = 10
+)
+
 type Config struct {
-	Address         string `env:"ADDRESS" envDefault:"localhost:8080"`
-	BackoffSchedule string `env:"BACKOFF_SCHEDULE" envDefault:"1,3,5"`
-	Key             string `env:"KEY" envDefault:""`
-	LogLevel        string `env:"LOG_LEVEL" envDefault:"info"`
-	PollInterval    int64  `env:"POLL_INTERVAL" envDefault:"2"`
-	ReportInterval  int64  `env:"REPORT_INTERVAL" envDefault:"10"`
+	Address         string `env:"ADDRESS"`
+	BackoffSchedule string `env:"BACKOFF_SCHEDULE"`
+	Key             string `env:"KEY"`
+	LogLevel        string `env:"LOG_LEVEL"`
+	PollInterval    int64  `env:"POLL_INTERVAL"`
+	RateLimit       int64  `env:"RATE_LIMIT"`
+	ReportInterval  int64  `env:"REPORT_INTERVAL"`
 }
 
 func NewConfig() (*Config, error) {
 	var cfg Config
+	flag.StringVar(&cfg.Address, "a", defaultAddress, "Server IP address and port")
+	flag.StringVar(&cfg.BackoffSchedule, "b", defaultBackoffSchedule, "Backoff schedule in seconds separated by commas")
+	flag.StringVar(&cfg.Key, "k", defaultKey, "Key for HMAC hash")
+	flag.StringVar(&cfg.LogLevel, "ll", defaultLogLevel, "Log level: info, error, fatal")
+	flag.Int64Var(&cfg.PollInterval, "p", defaultPollInterval, "Polling interval in seconds")
+	flag.Int64Var(&cfg.RateLimit, "l", defaultRateLimit, "Limit of requests")
+	flag.Int64Var(&cfg.ReportInterval, "r", defaultReportInterval, "Report interval in seconds")
+	flag.Parse()
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
-	flag.StringVar(&cfg.Address, "a", cfg.Address, "Server IP address and port")
-	flag.StringVar(&cfg.BackoffSchedule, "b", cfg.BackoffSchedule, "Backoff schedule in seconds separated by commas")
-	flag.StringVar(&cfg.Key, "k", cfg.Key, "Key for HMAC hash")
-	flag.StringVar(&cfg.LogLevel, "l", cfg.LogLevel, "Log level: info, error, fatal")
-	flag.Int64Var(&cfg.PollInterval, "p", cfg.PollInterval, "Polling interval in seconds")
-	flag.Int64Var(&cfg.ReportInterval, "r", cfg.ReportInterval, "Report interval in seconds")
-	flag.Parse()
 	if !strings.HasPrefix(cfg.Address, "http://") {
 		cfg.Address = "http://" + cfg.Address
 	}
