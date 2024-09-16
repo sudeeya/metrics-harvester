da49a8c091d06f61824fdce3bee341ee01f61519
sudeeya <pyrkin.aa@phystech.edu>
2024-09-17 00:29:37 +0300

    Add simple, stylecheck and quickfix analyzers to multichecker

--- internal/staticlint/staticcheck.go
+++ internal/staticlint/staticcheck.go
@@ -2,7 +2,10 @@ package staticlint
 
 import (
 	"golang.org/x/tools/go/analysis"
+	"honnef.co/go/tools/quickfix"
+	"honnef.co/go/tools/simple"
 	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
 )
 
 func ListStaticcheck() []*analysis.Analyzer {
@@ -10,5 +13,27 @@ func ListStaticcheck() []*analysis.Analyzer {
 	for _, v := range staticcheck.Analyzers {
 		analyzers = append(analyzers, v.Analyzer)
 	}
+
+	for _, v := range simple.Analyzers {
+		if v.Analyzer.Name == "S1000" {
+			analyzers = append(analyzers, v.Analyzer)
+			break
+		}
+	}
+
+	for _, v := range stylecheck.Analyzers {
+		if v.Analyzer.Name == "ST1000" {
+			analyzers = append(analyzers, v.Analyzer)
+			break
+		}
+	}
+
+	for _, v := range quickfix.Analyzers {
+		if v.Analyzer.Name == "QF1001" {
+			analyzers = append(analyzers, v.Analyzer)
+			break
+		}
+	}
+
 	return analyzers
 }
